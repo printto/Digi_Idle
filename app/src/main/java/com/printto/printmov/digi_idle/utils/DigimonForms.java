980152e93be4d9fdbba6cee05bda45dc303bc228
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2019-03-18 22:14:22 +0700

    Preparing Digivolve Activity

---
+++ app/src/main/java/com/printto/printmov/digi_idle/utils/DigimonForms.java
@@ -0,0 +1,10 @@
+package com.printto.printmov.digi_idle.utils;
+
+public class DigimonForms {
+
+    public static final int INTRAINING = 0,
+            ROOKIE = 1,
+            CHAMPION = 2,
+            ULTIMATE = 3,
+            MEGA = 4;
+}
